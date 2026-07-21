27665f1071d2315a12cdfc704d1380c06d983835
Zainab Fayyaz <zainabfayyaz207@gmailcom>
2026-07-22 02:06:38 +0500

    Update CORS policy in backend to support dynamic frontend origins like Vercel

--- DisasterPreparedness&ResponseSystem/DisasterPreparedness&ResponseSystem.API/Program.cs
+++ DisasterPreparedness&ResponseSystem/DisasterPreparedness&ResponseSystem.API/Program.cs
@@ -241,10 +241,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins(
-                "http://localhost:5173",    
-                "http://localhost:3000"    
-              )
+        policy.SetIsOriginAllowed(origin => true)
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();         
