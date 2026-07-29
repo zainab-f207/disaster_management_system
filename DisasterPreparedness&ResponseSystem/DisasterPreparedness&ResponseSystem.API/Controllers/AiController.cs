1817f4b56233e89f342c01197efd20c3df3c2406
Zainab Fayyaz <zainabfayyaz207@gmailcom>
2026-07-30 02:39:51 +0500

    fix: allow anonymous access to AI chat endpoint to fix 404/401 errors

--- DisasterPreparedness&ResponseSystem/DisasterPreparedness&ResponseSystem.API/Controllers/AiController.cs
+++ DisasterPreparedness&ResponseSystem/DisasterPreparedness&ResponseSystem.API/Controllers/AiController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -11,6 +12,7 @@ namespace DisasterPreparedness_ResponseSystem.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [AllowAnonymous]
     public class AiController : ControllerBase
     {
         private readonly IConfiguration _config;
